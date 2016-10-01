7902ce689c4715fa1c34c73fb70bccea03cb0af7
Kaito Udagawa <umireon@gmail.com>
2016-10-02 03:42:25 +0900

    Use provided makefile to build liquid-dsp

--- setup.py
+++ setup.py
@@ -13,17 +13,22 @@ from distutils.command.build_clib import build_clib as _build_clib
 
 import subprocess
 import os
+import shutil
 import numpy as np
 
 class build_clib(_build_clib):
     def configure_liquid(self):
-        SOURCE_DIR = os.path.join(os.getcwd(), 'liquid-dsp')
-        subprocess.call(['sh', 'bootstrap.sh'], cwd=SOURCE_DIR)
-        subprocess.call(['sh', 'configure', '--enable-simdoverride', '--prefix='], cwd=SOURCE_DIR)
+        subprocess.call(['sh', 'bootstrap.sh'], cwd='liquid-dsp')
+        subprocess.call(['sh', 'configure'], cwd='liquid-dsp')
+        subprocess.call(['make', 'libliquid.a'], cwd='liquid-dsp')
+
+    def install_liquid(self):
+        shutil.copyfile('liquid-dsp/libliquid.a', os.path.join(self.build_clib, 'libliquid.a'))
 
     def build_libraries(self, libraries):
         self.configure_liquid()
         _build_clib.build_libraries(self, libraries)
+        self.install_liquid()
 
 extensions = cythonize([
     Extension('pyliquiddsp.libliquid',
@@ -31,12 +36,10 @@ extensions = cythonize([
     )
 ])
 
-libraries = [
-    ('liquid', {
-        'include_dirs': ['liquid-dsp', 'liquid-dsp/include'],
-        'sources': open('liquid-dsp.files').read().splitlines(),
-    })
-]
+libraries = [('liquid', {
+    'sources': ['liquid-dsp/src/libliquid.c'],
+    'include_dirs': ['liquid-dsp', 'liquid-dsp/include'],
+})]
 
 setup(
     name='pyliquiddsp',
